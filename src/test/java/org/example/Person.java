2291ed386a2ccb0a9fc24e5b3e7a2915b6426575
Helena <133242692+HellyGH@users.noreply.github.com>
2023-11-28 12:03:34 +0200

    Create Person.java

---
+++ src/test/java/org/example/Person.java
@@ -0,0 +1,26 @@
+package org.example;
+
+import org.junit.Test;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class PersonTest {
+    @Test
+    public void testConstructorWithParameters() {
+        var p = new Person("Helly", 20);
+        assertEquals("Helly", p.getName());
+        assertEquals(20,p.getAge());
+
+    }
+    @Test
+    public void testPrintDetails() {
+
+
+
+    }
+
+}
