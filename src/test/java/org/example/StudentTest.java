7252bf61f59fff7af196a743cb20385a17d969a6
Helena <133242692+HellyGH@users.noreply.github.com>
2024-01-18 14:22:50 +0200

    initial commit

--- src/test/java/org/example/StudentTest.java
+++ src/test/java/org/example/StudentTest.java
@@ -7,7 +7,31 @@ import static org.junit.Assert.assertNull;
 import static org.junit.jupiter.api.Assertions.*;
 
 
+
+
 public class StudentTest {
+
+    @Test
+    @ParameterizedTest
+    public void testStudentPrintDetailsMethod() {
+        Student student = new Student("John Doe", 20, 85.5);
+
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+
+        student.PrintDetails();
+
+
+        System.setOut(System.out);
+
+        String expectedOutput = "Student: John Doe age:20 grade:85.5" + System.lineSeparator();
+        assertEquals(expectedOutput, outContent.toString());
+    }
+
+  
+}
+
+
 
 
 
